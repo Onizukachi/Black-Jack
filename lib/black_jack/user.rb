681daa6d3684c2ffaa28ae9cd396eb00ebf19a63
Onizukachi <dark_sao@mail.ru>
2024-11-12 20:15:09 +0300

    mini fix in User class

--- lib/black_jack/user.rb
+++ lib/black_jack/user.rb
@@ -4,7 +4,7 @@ module BlackJack
       action_mapping = { 1 => :skip, 2 => :take_card, 3 => :show_cards }
 
       chosen_action = nil
-      chosen_action = Interface.choose_action(self) until action_mapping.keys.include?(chosen_action)
+      chosen_action = Interface.choose_action(self) until action_mapping[chosen_action]
 
       action_mapping[chosen_action]
     end
